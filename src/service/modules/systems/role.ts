959c9015605ccb51379adf23fd751a08940da79d
coderzzx <2549518530@qq.com>
2024-07-19 14:19:49 +0800

    Mod -> role -> 调整角色代码

--- src/service/modules/systems/role.ts
+++ src/service/modules/systems/role.ts
@@ -1,6 +1,11 @@
 import VrRequest from '@/service';
-import { IRoleListParams, IRoleList, ICreateRoleParams, IUpdateRoleParams, IRoleInfo } from '@/types/systems/role';
+import { IRoleListParams, IRoleList, ICreateRoleBody, IUpdateRoleBody, IRoleInfo } from '@/types/systems/role';
 
+/**
+ * 获取角色列表
+ * @param {IRoleListParams} data 查询参数
+ * @returns
+ */
 export const getRoleListService = (data: IRoleListParams) => {
   return VrRequest.get<IRoleList>({
     url: '/roles/role-list',
@@ -8,20 +13,34 @@ export const getRoleListService = (data: IRoleListParams) => {
   });
 };
 
-export const createRoleService = (data: ICreateRoleParams) => {
+/**
+ * 添加角色
+ * @param {ICreateRoleBody} data 添加角色参数
+ * @returns {Promise<void>}
+ */
+export const createRoleService = (data: ICreateRoleBody) => {
   return VrRequest.post({
     url: '/roles/create-role',
     data
   });
 };
 
-export const updateRoleService = (data: IUpdateRoleParams) => {
+/**
+ * 编辑角色
+ * @param {IUpdateRoleBody} data 编辑角色参数
+ * @returns {Promise<void>}
+ */
+export const updateRoleService = (data: IUpdateRoleBody) => {
   return VrRequest.post({
     url: '/roles/update-role',
     data
   });
 };
 
+/**
+ * 获取所有角色列表
+ * @returns {Promise<IRoleInfo[]>} 角色列表
+ */
 export const getAllRoleListService = () => {
   return VrRequest.get<IRoleInfo[]>({
     url: '/roles/all-role-list'
