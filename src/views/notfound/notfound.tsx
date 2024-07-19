344234edfb230844562e906baa3962cf442c4d19
coderzzx <2549518530@qq.com>
2024-07-19 16:49:45 +0800

    Add -> view -> notfound

--- src/views/notfound/notfound.tsx
+++ src/views/notfound/notfound.tsx
@@ -1,12 +1,33 @@
 import { memo } from 'react';
 import type { FC, ReactNode } from 'react';
 
+import { Button, Result } from 'antd';
+import NotFoundWrapper from './style';
+import { useNavigate } from 'react-router-dom';
+
 interface IProps {
   children?: ReactNode;
 }
 
 const notfound: FC<IProps> = () => {
-  return <div>notfound</div>;
+  const navigate = useNavigate();
+  const onClickBackHome = () => {
+    navigate('/');
+  };
+  return (
+    <NotFoundWrapper>
+      <Result
+        status="404"
+        title="404"
+        subTitle="抱歉，您访问的页面不存在。"
+        extra={
+          <Button type="primary" onClick={onClickBackHome}>
+            回到首页
+          </Button>
+        }
+      />
+    </NotFoundWrapper>
+  );
 };
 
 export default memo(notfound);
